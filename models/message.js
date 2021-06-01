73e74124ba28c2a65ba36546ee11513dd8511783
Seraj Aldin Abo Sabbah <serajaldinabosabbah@gmail.com>
2021-06-01 00:36:33 -0700

    fixed time

--- models/message.js
+++ models/message.js
@@ -1,10 +1,10 @@
-const moment = require('moment');
+const moment = require('moment-timezone');
 
 function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format(' h:mm a')
+    time: moment().tz('America/Vancouver').format(' h:mm a')
   };
 }
 
